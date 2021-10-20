78ed3d47442470c48757c4f94cb233f8ebf28c14
yosuke0517 <yosuke.takeuchi.dev@gmail.com>
2021-10-20 22:45:14 +0900

    add 開発環境の構築

--- README.md
+++ README.md
@@ -7,3 +7,6 @@
 - DB: MySQL(5.7)
   - 8系とgoのライブラリの対応状況が不明確なので一旦5.7を使う
 - セッション管理: Redis
+
+# 環境構築
+- https://fuzzy-hunter-3bf.notion.site/golang-docker-afad7bfb494740b3910834a489452784
--- main.go
+++ main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"fmt"
 	"github.com/labstack/echo"
+	"net/http"
 )
+
+func Hello() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello World")
+	}
+}
 
 func main() {
 	fmt.Println("start")
 
 	e := echo.New()
+
+	// 仮ルーティング
+	e.GET("/", Hello())
 	e.Logger.Fatal(e.Start(":8080"))
 }
