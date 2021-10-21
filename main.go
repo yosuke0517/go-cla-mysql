c02f0776a6acdb7a104e20c6befddaeb79b0297e
yosuke0517 <yosuke.takeuchi.dev@gmail.com>
2021-10-21 22:55:28 +0900

    add Swagger用設定追記・routingを追記

--- main.go
+++ main.go
@@ -3,21 +3,22 @@ package main
 import (
 	"fmt"
 	"github.com/labstack/echo"
-	"net/http"
+	"go-cla-mysql/routes"
 )
 
-func Hello() echo.HandlerFunc {
-	return func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello WWorld")
-	}
-}
-
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample swagger server.
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:8000
+// @BasePath /api/v1
 func main() {
 	fmt.Println("start")
 
 	e := echo.New()
 
-	// 仮ルーティング
-	e.GET("/", Hello())
+	// ルーティング
+	routes.InitRoutign(e)
 	e.Logger.Fatal(e.Start(":8080"))
 }
