8697fd70b8fc72385df1ba51f87ebf4905f91105
yosuke0517 <yosuke.takeuchi.dev@gmail.com>
2021-10-21 22:54:36 +0900

    add healthCheck用controller新規追加

---
+++ adapters/controllers/healthCheck.go
@@ -0,0 +1,18 @@
+package controllers
+
+import (
+	"github.com/labstack/echo"
+	"net/http"
+)
+
+func HealthCheckController() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.JSON(http.StatusOK, "I'm fine\n")
+	}
+}
+
+func HelloController() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.JSON(http.StatusOK, "Hello")
+	}
+}
