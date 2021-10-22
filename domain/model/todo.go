e035278d040e9fb0e714042ef8f4ff55ffac0b6f
yosuke0517 <yosuke.takeuchi.dev@gmail.com>
2021-10-22 22:24:50 +0900

    add todo構造体を作成

---
+++ domain/model/todo.go
@@ -0,0 +1,9 @@
+package model
+
+// TODO Specificationの定義（例…LimitDateが登録日（登録した日）より前の場合エラーを返す）
+type Todo struct {
+	ID        int    `json:"id"`
+	Task      string `json:"task"`
+	LimitDate string `json:"limitDate"`
+	Status    bool   `json:"status"`
+}
