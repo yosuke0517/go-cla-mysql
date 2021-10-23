092729dde85ad83f0915c8a5886ead7305365299
yosuke0517 <yosuke.takeuchi.dev@gmail.com>
2021-10-23 22:55:58 +0900

    add repositoryを新規追加

--- usecases/repository/todo.go
+++ usecases/repository/todo.go
@@ -4,7 +4,7 @@ import "go-cla-mysql/entities/model"
 
 // todoのCRUDに対するDB用のリポジトリ（ポート）
 type TodoRepository interface {
-	FindAll() (todos []*model.Todo, err error)
+	FindAll(max int) (todos []*model.Todo, err error)
 	FindByID(id int) (todos []*model.Todo, err error)
 	Create(todo *model.Todo) (*model.Todo, error)
 	Update(todo *model.Todo) (*model.Todo, error)
