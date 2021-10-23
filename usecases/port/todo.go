620abc45e66e97f7879dba4c01d7dbc916b4276e
yosuke0517 <yosuke.takeuchi.dev@gmail.com>
2021-10-23 22:54:44 +0900

    fix 各種メソッドの型修正

--- usecases/port/todo.go
+++ usecases/port/todo.go
@@ -3,24 +3,26 @@ package port
 import "go-cla-mysql/entities/model"
 
 // 現状はtodoを配列にしただけだが、今後子供のtodoとかを入れたい要望があったときとかの場合にここに追加していく
-type TodosUseCaseDto struct {
+type TodoOutPutUseCaseDto struct {
+	Hits  int `json:"hits"`
 	Todos []model.Todo
 }
 
-type TodoOutPutUseCaseDto struct {
-	Hits  int `json:"hits"`
-	Todos TodosUseCaseDto
+func NewTodoOutPutUseCaseDto(hits int, todos []model.Todo) *TodoOutPutUseCaseDto {
+	return &TodoOutPutUseCaseDto{
+		Hits:  hits,
+		Todos: todos,
+	}
 }
 
 type TodoInputPort interface {
-	Create() TodosUseCaseDto
-	FindAll() TodoOutPutUseCaseDto
-	FindByID(id int) (TodosUseCaseDto, error)
-	// Save(domain.Todoなどの単数系) (int64, error)
-	// DeleteByID(int) error
+	Create() model.Todo
+	FindAll(max int) model.Todo
+	FindByID(id int) (model.Todo, error)
+	Update(todo model.Todo) (model.Todo, error)
 }
 
 type TodoOutputPort interface {
-	Render(*model.Todo)
+	Render(todos []*model.Todo)
 	RenderError(error)
 }
