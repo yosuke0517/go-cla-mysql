778ac71e8aeefec80dbb6bb8e1c7d3f48d01eab7
yosuke0517 <yosuke.takeuchi.dev@gmail.com>
2021-10-25 23:46:26 +0900

    add 新規作成

---
+++ usecases/dto/todo.go
@@ -0,0 +1,16 @@
+package dto
+
+import "go-cla-mysql/entities/model"
+
+// 現状はtodoを配列にしただけだが、今後子供のtodoとかを入れたい要望があったときとかの場合にここに追加していく
+type TodoOutPutUseCaseDto struct {
+	Hits  int `json:"hits"`
+	Todos model.Todos
+}
+
+func NewTodoOutPutUseCaseDto(hits int, todos []model.Todo) *TodoOutPutUseCaseDto {
+	return &TodoOutPutUseCaseDto{
+		Hits:  hits,
+		Todos: todos,
+	}
+}
