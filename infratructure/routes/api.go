17311935f8b170e75bcd304cedd1288f3537cf0d
yosuke0517 <yosuke.takeuchi.dev@gmail.com>
2021-10-27 19:00:39 +0900

    fix GetAllのroutingを追加

--- infratructure/routes/api.go
+++ infratructure/routes/api.go
@@ -3,12 +3,15 @@ package routes
 import (
 	"github.com/labstack/echo"
 	"go-cla-mysql/adapters/controllers"
+	"go-cla-mysql/injector"
 )
 
 func InitRoutign(e *echo.Echo) {
+	todo := injector.InjectTodo()
 	e.GET("/health", controllers.HealthCheckController())
 	g := e.Group("/api/v1")
 	{
 		g.GET("/hello", controllers.HelloController())
+		g.GET("/todo/get", todo.GetAll())
 	}
 }
