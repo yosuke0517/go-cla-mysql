e83a79355e854c4053f8625bb36aec4a356db8e3
yosuke0517 <yosuke.takeuchi.dev@gmail.com>
2021-10-28 20:10:30 +0900

    fix httpメソッドのヘルパーを作成

---
+++ routes/api.go
@@ -0,0 +1,49 @@
+package routes
+
+import (
+	"go-cla-mysql/adapters/controllers"
+	"go-cla-mysql/injector"
+	"net/http"
+)
+
+func InitRoutign() {
+	todo := injector.InjectTodo()
+	http.HandleFunc("/health", get(controllers.HealthCheckController()))
+	http.HandleFunc("/hello", get(controllers.HelloController()))
+	http.HandleFunc("/api/v1/todo/get", todo.GetAll())
+}
+
+// get GETリクエストを処理する
+func get(apiFunc http.HandlerFunc) http.HandlerFunc {
+	return httpMethod(apiFunc, http.MethodGet)
+}
+
+// post POSTリクエストを処理する
+func post(apiFunc http.HandlerFunc) http.HandlerFunc {
+	return httpMethod(apiFunc, http.MethodPost)
+}
+
+// httpMethod 指定したHTTPメソッドでAPIの処理を実行する
+func httpMethod(apiFunc http.HandlerFunc, method string) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+
+		// CORS対応
+		writer.Header().Add("Access-Control-Allow-Origin", "*")
+		writer.Header().Add("Access-Control-Allow-Headers", "Content-Type,Accept,Origin,x-token")
+
+		// プリフライトリクエストは処理を通さない
+		if request.Method == http.MethodOptions {
+			return
+		}
+		// 指定のHTTPメソッドでない場合はエラー
+		if request.Method != method {
+			writer.WriteHeader(http.StatusMethodNotAllowed)
+			writer.Write([]byte("Method Not Allowed"))
+			return
+		}
+
+		// 共通のレスポンスヘッダを設定
+		writer.Header().Add("Content-Type", "application/json")
+		apiFunc(writer, request)
+	}
+}
