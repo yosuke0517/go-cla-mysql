15f4712ad6a6138b5d3aa2ab2f619872763d2dab
yosuke0517 <yosuke.takeuchi.dev@gmail.com>
2021-10-29 10:46:46 +0900

    add 新規作成（MySQL）

--- infratructure/db/sqlHandler.go
+++ infratructure/db/sqlHandler.go
@@ -3,6 +3,7 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"os"
 )
 
@@ -13,6 +14,7 @@ type SqlHandler struct {
 func NewSqlHandler() *SqlHandler {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
 	conn, err := sql.Open("mysql", dsn)
+
 	if err != nil {
 		panic(err.Error)
 	}
