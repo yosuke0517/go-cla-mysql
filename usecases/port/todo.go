d499fe555a621090667de1b92e20941df0fa9ad5
yosuke0517 <yosuke.takeuchi.dev@gmail.com>
2021-10-30 11:04:10 +0900

    fix #6 updateの引数の修正

--- usecases/port/todo.go
+++ usecases/port/todo.go
@@ -10,9 +10,10 @@ type TodoInputPort interface {
 	Create(todo *model.Todo) (bool, error)
 	FindAll(ctx context.Context, max int) (*dto.TodoOutPutUseCaseDto, error)
 	FindByID(ctx context.Context, id int) (*dto.TodoOutPutUseCaseDto, error)
-	Update(ctx context.Context, todo model.Todo) (model.Todos, error)
+	Update(todo *model.Todo) (bool, error)
 }
 
+// TODO 共通のレスポンスの処理入れているのでいらないかもしれない…
 type TodoOutputPort interface {
 	Render(todos *model.Todos) dto.TodoOutPutUseCaseDto
 	RenderError(error)
