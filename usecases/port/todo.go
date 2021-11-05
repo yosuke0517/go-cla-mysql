b66fc49c62ead50ce0c4315b7ffad1d451069305
yosuke0517 <yosuke.takeuchi.dev@gmail.com>
2021-11-05 23:40:12 +0900

    fix http.ResponseWriterを引数から除外

--- usecases/port/todo.go
+++ usecases/port/todo.go
@@ -2,18 +2,17 @@ package port
 
 import (
 	"go-cla-practice/entities/model"
-	"net/http"
 )
 
 type TodoInputPort interface {
-	Create(writer http.ResponseWriter, todo *model.Todo)
-	FindAll(writer http.ResponseWriter, max int)
-	FindByID(writer http.ResponseWriter, id int)
-	Update(writer http.ResponseWriter, todo *model.Todo)
+	Create(todo *model.Todo)
+	FindAll(max int)
+	FindByID(id int)
+	Update(todo *model.Todo)
 }
 
 // TodoOutputPort この中で共通のレスポンス処理を呼ぶ
 type TodoOutputPort interface {
-	Render(writer http.ResponseWriter, todos *model.Todos)
-	RenderError(writer http.ResponseWriter, err error)
+	Render(todos *model.Todos)
+	RenderError(err error)
 }
