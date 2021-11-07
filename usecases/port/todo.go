18e99cfc4168b81f54af81ccb440d2f2019d79b2
yosuke0517 <yosuke.takeuchi.dev@gmail.com>
2021-11-07 14:33:14 +0900

    fix #14 返り値を修正

--- usecases/port/todo.go
+++ usecases/port/todo.go
@@ -6,14 +6,15 @@ import (
 )
 
 type TodoInputPort interface {
-	Create(todo *model.Todo) (bool, error)
-	FindAll(max int) (dto.TodoOutPutUseCaseDto, error)
-	FindByID(id int) (dto.TodoOutPutUseCaseDto, error)
-	Update(todo *model.Todo) (bool, error)
+	Create(todo *model.Todo) (*dto.TodoOutPutUseCaseDto2, error)
+	FindAll(max int) (*dto.TodoOutPutUseCaseDto, error)
+	FindByID(id int) (*dto.TodoOutPutUseCaseDto, error)
+	Update(todo *model.Todo) (*dto.TodoOutPutUseCaseDto2, error)
 }
 
+// TODO intreractorの中でoutputport呼ぶだけにしたいけどresponseWriter渡さないといけなくて、テストしづらいので断念
 // TodoOutputPort この中で共通のレスポンス処理を呼ぶ
-type TodoOutputPort interface {
-	Render(todos *model.Todos)
-	RenderError(err error)
-}
+//type TodoOutputPort interface {
+//	Render(todos *model.Todos)
+//	RenderError(err error)
+//}
